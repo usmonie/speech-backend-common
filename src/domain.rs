c8f4bee178799a51420fa9f5fdad07f6b8e73c7f
Usman Akhmedov <nie@speech.wtf>
2023-03-15 23:14:24 +0300

    Updated error.rs

--- src/domain.rs
+++ src/domain.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use crate::models::ApiError;
+use crate::error::ApiError;
 
 #[async_trait]
 pub trait AbstractUseCase<R, T> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
 pub mod domain;
-pub mod models;
+pub mod error;
